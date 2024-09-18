25ff7ceb6d2e01d50f96d48f1b58dc1daf04ac36
Bhuvanesh <148686860+P-Bhuvanesh@users.noreply.github.com>
2024-09-18 14:17:03 +0530

    Use of HashMap for HT

--- Hash Table/Built-in_HashMap.java
+++ Hash Table/Built-in_HashMap.java
@@ -5,15 +5,15 @@ class Main{
     {
         HashMap<String, Integer> map = new HashMap<>();
         
-        map.put("Alice",29);
+        map.put("Alice",29); //inserted to hashmap
         map.put("Sundara",62);
         map.put("Nazim",15);
         
-        System.out.println(map);
+        System.out.println(map);// Display of map table
         
-        System.out.println(map.get("Alice"));
+        System.out.println(map.get("Alice"));// Retrieval of value
         
-        map.remove("Alice");
+        map.remove("Alice");// Deletion of key-value pair
         System.out.println(map);
     }
 }
