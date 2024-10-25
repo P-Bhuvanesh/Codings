ed5eef102f94b68bf692cf91a39b7869eae361b4
Bhuvanesh <148686860+P-Bhuvanesh@users.noreply.github.com>
2024-10-25 07:47:46 +0530

    Resolved error

--- Sorting/Insertion.java
+++ Sorting/Insertion.java
@@ -5,17 +5,23 @@ public class Main
 	public static void main(String[] args) {
 		int arr[] =  {30, 10, 50, 20, 40, 60};
 		// Insertion Sort 
-		for(int i=1;i<arr.length;i++)
+		// 10 30 50 20 40 60
+		// 10 30 50 20 40 60
+		for(int i=1;i<arr.length;i++) // i=1 || i = 2 || i = 3
 		{
-		    int j = i-1;
-		    int key = arr[i];
+		    int j = i-1; // j = 0 || j =1 || j = 2
+		    int key = arr[i]; // key = 10 || key = 50 || key = 20
 		    
-		    while(j >= 0 && arr[j] < key)
+		    while(j >= 0 && arr[j] > key) // 30>10 || 30>50 || 50>20 -> 30>20 -> 1>20||
 		    {
-		        arr[j+1] = arr[j]; // Swapping
-		        j = j-1;
+		        arr[j+1] = arr[j]; // arr[1]=30 || arr[3]=50 -> arr[2]=30
+		        j = j-1; // j=-1 || j = 1 -> j = 0 
 		    }
-		    arr[j+1] = key;
+		    arr[j+1] = key; // arr[0] = 10 || arr[2] = 50 || arr[1] = 20
+			// 10 30 50 20 40 60
+			// 10 30 50 20 40 60
+			// 10 20 30 50 40 60
+			
 		}
 		System.out.println(Arrays.toString(arr));
 	}
