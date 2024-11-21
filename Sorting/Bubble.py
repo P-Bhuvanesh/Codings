81d99f5f9af2a9b1e7662097b66a21ae0eea761f
Bhuvanesh <148686860+P-Bhuvanesh@users.noreply.github.com>
2024-11-21 11:19:24 +0530

    Updated Bubble sorting

--- Sorting/Bubble.py
+++ Sorting/Bubble.py
@@ -1,10 +1,32 @@
-l = [34, 56, 73, 90, 81, 27]
+# Optimized Python program for implementation of Bubble Sort
+def bubbleSort(arr):
+    n = len(arr)
+    
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
 
-for i in range(len(l) - 1):
-    for j in range(len(l) - 1 - i):
-        if l[j] > l[j + 1]:
-            l[j], l[j + 1] = l[j + 1], l[j]
-print(l)
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if (swapped == False):
+            break
+
+# Driver code to test above
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+
+    bubbleSort(arr)
+
+    print("Sorted array:")
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
 
 """
 Bubble sort : It is that comparing the adjacent element and swaping if the condition trues a<b || a>b 
